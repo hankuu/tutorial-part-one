d456f7198f83abea88a049a1403f5d9017f6312e
hankuu <hanna.kumpula@gmail.com>
2018-09-14 05:23:19 +0300

    Eka commit

---
+++ src/pages/counter.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Counter extends React.Component {
+  //tää suoritetaan vaan alussa vai?
+  constructor() {
+    console.log("konstruktor")
+  super()
+  this.state = { count: 0 }
+}
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>current count: {this.state.count}</p>
+
+        <button
+          onClick={() =>
+            this.setState(prevState => ({
+              count: prevState.count + 1,
+            }))
+          }
+        >
+          plus
+        </button>
+
+        <button
+          onClick={() =>
+            this.setState(prevState => ({
+              count: prevState.count - 1,
+            }))
+          }
+        >
+          minus
+        </button>
+        <Link to="/"> etusivulle</Link>
+      </div>
+    )
+    }
+}
+
+export default Counter;
